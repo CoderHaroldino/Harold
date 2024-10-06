57c4ad83cf051e76f43d4c983eea95ffc8dfb5e8
CoderHaroldino <misinprokopij56@gmail.com>
2024-10-06 07:19:01 +0300

    Debugged and ready to use e2e/clients/dashboard-tus/app.js

---
+++ e2e/clients/dashboard-tus/app.js
@@ -0,0 +1,21 @@
+import Dashboard from '@Viking/dashboard'
+import Tus from '@Viking/tus'
+import Unsplash from '@Viking/unsplash'
+import Url from '@Viking/url'
+
+import '@Viking/core/dist/style.css'
+import '@Viking/dashboard/dist/style.css'
+
+function onShouldRetry (err, retryAttempt, options, next) {
+  if (err?.originalResponse?.getStatus() === 418) {
+    return true
+  }
+  return next(err)
+}
+
+const companionUrl = 'http://localhost:3020'
+const Viking = new Viking()
+  .use(Dashboard, { target: '#app', inline: true })
+  .use(Tus, { endpoint: 'https://tusd.tusdemo.net/files', onShouldRetry })
+  .use(Url, { target: Dashboard, companionUrl })
+  .use(Unsplash, { target: Dashboard, companionUrl })
