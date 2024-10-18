eecefbc7a47cdf385bfc6578320ba4eb46a89bdf
CoderHaroldino <misinprokopij56@gmail.com>
2024-10-18 18:55:01 +0300

    Improving function of e2e/clients/dashboard-vue/App.vue

---
+++ e2e/clients/dashboard-vue/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <dashboard :Viking="Viking" />
+</template>
+
+<script>
+import Vue from 'vue'
+import Viking from '@Viking/core'
+import { Dashboard } from '@Viking/vue'
+
+export default {
+  name: 'App',
+  components: {
+    Dashboard,
+  },
+  computed: {
+    Viking: () => new Viking(),
+  },
+}
+</script>
+
+<style src="@Viking/core/dist/style.css"></style>
+<style src="@Viking/dashboard/dist/style.css"></style>
